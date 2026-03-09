d037b4faa781e07b857c34142881a9fc080caa71
Thannob Aribarg <iamaribarg@gmail.com>
2026-03-09 17:11:46 +0700

    change text

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.delete('/users', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('CORS-enabled web server listening on port 3000')
+    console.log('CORS-enabled web server listening on port 3000 or other port')
 })
 
 // export the app for vercel serverless functions
